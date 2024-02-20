697719dcd1b60d6a98a74c6b029efe639f51d6f9
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2024-02-20 18:14:07 +0100

    modifie blade pages

--- resources/views/layout/_shared.blade.php
+++ resources/views/layout/_shared.blade.php
@@ -18,7 +18,7 @@
 
     <!--Start Navbar tailwind -->
     <nav
-        class="flex items-center justify-between flex-wrap  py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
+        class="flex items-center justify-between flex-wrap  py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700 border-b-2 border-blue-700 mb-4">
         <div
             class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
             <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,35 @@
 @extends('layout._shared')
 
 @section('content')
+
+
     
+
+<section
+  class="relative  bg-[url(https://cdn.pixabay.com/photo/2024/01/12/13/00/field-8503934_960_720.jpg)] bg-cover bg-center bg-no-repeat main-news"
+>
+  <div
+    class="absolute inset-0"
+  ></div>
+
+  <div
+    class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+  >
+    <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+      <h1 class="text-3xl font-extrabold sm:text-5xl">
+        Let us find your
+         <div class="mt-8 flex flex-wrap gap-4 text-center">
+        <a
+          href="#"
+          class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+        >
+         Mehr &#x3e;&#x3e;
+        </a>
+
+      
+      </div>
+    </div>
+  </div>
+</section>
+
 @endsection
