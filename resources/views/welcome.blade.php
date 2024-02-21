af0b9b61a30138723278a208dc8c4038b70f634a
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2024-02-21 14:20:04 +0100

    MODIFI CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,9 @@
     <ol class="coverflow-list">
       <!-- Cover item -->
        @foreach ($data as $item)
-      <input type="radio" name="cover-item" id="cover-{{ $item['_id'] }}">
+      <input type="radio" name="cover-item" id="cover-{{ $item['_id'] }}" @if ( $item['_id']  === "660b1b34a596c63dd27b08b5")
+checked="checked"
+@endif>
       <li class="coverflow-item">
         <label for="cover-{{ $item['_id'] }}">
           <figure class="album-cover">
@@ -29,20 +31,22 @@
 <!--##################################################################################################-->
 
 <!--START news item -->
+ @foreach ($data as $item)
 <div class="main-container2">
   <div class="coverflow-container2">
     <div class="coverflow-list2">
       <div class="coverflow-item2">
         <label  class="label2">
           <figure class="album-cover2">
-            <img  class="img2"  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3211/51psxBugNkL.jpg">
-            <figcaption class="album-name2">Love Is Blind</figcaption>
+            <img  class="img2"  src="{{ $item['image'] }}">
+            <figcaption class="album-name2 lg:text-3xl sm:text-xl">{{$item['title']}}</figcaption>
           </figure>
         </label>
       </div>
     </div>
   </div>
   </div>
+   @endforeach
 <!-- END news item -->
 
 <!--##################################################################################################-->
