15daaa505724e7289845ecdf9d55f3932ff2492b
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2024-02-22 15:19:44 +0100

    MainNewsController is done

--- app/Http/Controllers/MainNewsController.php
+++ app/Http/Controllers/MainNewsController.php
@@ -2,55 +2,184 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\WeatherService;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MainNewsController extends Controller
 {
+protected $weatherService;
+
+    public function __construct(WeatherService $weatherService)
+    {
+        $this->weatherService = $weatherService;
+    }
+
     public function news()   {
 
-            return view('main-news.news');
-    }
+        // Make a GET request to the external API
+        $response = Http::get('http://localhost:3000/api/v1/news');
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
+
+            // Pass the data to the view
+            return view('main-news.news', ['data' => $data['data']]);
+    }    else {
+            // Handle the error if the request fails
+            abort(500, 'Failed to fetch data from the API');
+        }}
     //#######################################
+
+
     public function businuss()   {
 
-            return view('main-news.businuss');
-    }
-    //#######################################
-    public function gesundheit()   {
+        // Make a GET request to the external API
+        $response = Http::get('http://localhost:3000/api/v1/news');
 
-            return view('main-news.gesundheit');
-    }
-    //#######################################
-    public function politik()   {
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
 
-            return view('main-news.politik');
-    }
+            // Pass the data to the view
+            return view('main-news.businuss', ['data' => $data['data']]);
+    }    else {
+            // Handle the error if the request fails
+            abort(500, 'Failed to fetch data from the API');
+        }}
     //#######################################
+
+
+
+    public function gesundheit()  {
+
+        // Make a GET request to the external API
+        $response = Http::get('http://localhost:3000/api/v1/news');
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
+
+            // Pass the data to the view
+            return view('main-news.gesundheit', ['data' => $data['data']]);
+    }    else {
+            // Handle the error if the request fails
+            abort(500, 'Failed to fetch data from the API');
+        }}
+    //#######################################
+
+
+    public function politik()  {
+
+        // Make a GET request to the external API
+        $response = Http::get('http://localhost:3000/api/v1/news');
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
+
+            // Pass the data to the view
+            return view('main-news.politik', ['data' => $data['data']]);
+    }    else {
+            // Handle the error if the request fails
+            abort(500, 'Failed to fetch data from the API');
+        }}
+    //#######################################
+
+
       public function reise()   {
 
-            return view('main-news.reise');
-    }
+        // Make a GET request to the external API
+        $response = Http::get('http://localhost:3000/api/v1/news');
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
+
+            // Pass the data to the view
+            return view('main-news.reise', ['data' => $data['data']]);
+    }    else {
+            // Handle the error if the request fails
+            abort(500, 'Failed to fetch data from the API');
+        }}
     //#######################################
+
+
          public function sport()   {
 
-            return view('main-news.sport');
-    }
+        // Make a GET request to the external API
+        $response = Http::get('http://localhost:3000/api/v1/news');
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
+
+            // Pass the data to the view
+            return view('main-news.sport', ['data' => $data['data']]);
+    }    else {
+            // Handle the error if the request fails
+            abort(500, 'Failed to fetch data from the API');
+        }}
     //#######################################
+
+
             public function video()   {
 
-            return view('main-news.video');
-    }
+        // Make a GET request to the external API
+        $response = Http::get('http://localhost:3000/api/v1/news');
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
+
+            // Pass the data to the view
+            return view('main-news.video', ['data' => $data['data']]);
+    }    else {
+            // Handle the error if the request fails
+            abort(500, 'Failed to fetch data from the API');
+        }}
     //#######################################
 
-           public function wetter()   {
 
-            return view('main-news.wetter');
-    }
+
+           public function wetter(Request $request)   {
+        //openWeatherMap Api
+        $city = $request->input('city');
+        $weatherData = $this->weatherService->getWeather($city);
+
+        // Make a GET request to the external API
+        $response = Http::get('http://localhost:3000/api/v1/news');
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
+
+            // Pass the data to the view
+            return view('main-news.wetter', ['data' => $data['data']],['weatherData' => $weatherData]);
+    }    else {
+            // Handle the error if the request fails
+            abort(500, 'Failed to fetch data from the API');
+        }}
     //#######################################
+
+
          public function ticker()   {
 
-            return view('main-news.ticker');
-    }
+        // Make a GET request to the external API
+        $response = Http::get('http://localhost:3000/api/v1/news');
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            $data = $response->json();
+
+            // Pass the data to the view
+            return view('main-news.ticker', ['data' => $data['data']]);
+    }    else {
+            // Handle the error if the request fails
+            abort(500, 'Failed to fetch data from the API');
+        }}
     //#######################################
 
 
--- resources/views/main-news/businuss.blade.php
+++ resources/views/main-news/businuss.blade.php
@@ -1,36 +1,27 @@
-@extends('layout._shared')
+@extends('layout._mainnews')
 
-@section('content')
-    <section class="mb-40">
-        <!-- Jumbotron -->
-        <div class="relative overflow-hidden bg-cover bg-no-repeat"
-            style="
-        background-position: 50%;
-        background-image: url('https://cdn.pixabay.com/photo/2023/11/23/20/40/ocean-8408693_1280.jpg');
-        height: 500px;
-      ">
-            <div
-                class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.75)] bg-fixed">
-                <div class="flex h-full items-center justify-center">
-                    <div class="px-6 text-center text-white md:px-12">
-                        <h1 class="mt-2 mb-16 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">
-                            Politik </h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Jumbotron -->
+@section('title')
+   Business 
+@endsection
 
+{{--###########################################################--}}
 
+@section('img')
 
-    </section>
+https://cdn.pixabay.com/photo/2024/02/09/16/17/grey-whale-8563340_960_720.png
 
+@endsection
 
-  <!-- cards news -->
+{{--###########################################################--}}
+
+@section('news')
+<!-- cards news -->
     <div class="container my-12 mx-auto px-4 md:px-12">
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
-
-            <!-- Column -->
+          
+ @foreach ($data as $item)
+  @if ($item['sort']=== "Business" )
+            <!-- Column --> 
             <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
 
                 <!-- Article -->
@@ -38,13 +29,13 @@
 
                     <a href="#">
                         <img alt="Placeholder" class="block h-auto w-full"
-                            src="https://cdn.pixabay.com/photo/2023/11/23/20/40/ocean-8408693_1280.jpg">
+                            src="{{ $item['image'] }}">
                     </a>
 
                     <header class="flex items-center justify-between leading-tight p-2 md:p-4">
                         <h1 class="text-lg">
                             <a class="no-underline hover:underline  text-white" href="#">
-                                Article Title
+                               {{ $item['title'] }}
                             </a>
                         </h1>
                         <p class="text-white text-sm">
@@ -67,7 +58,12 @@
 
             </div>
             <!-- END Column -->
+ @endif 
+    @endforeach
+ 
         </div>
     </div>
       <!-- END cards news -->
+
+    
 @endsection
